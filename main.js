b40e5bb470a556487e7c03ec1aa3e8ffaace2da1
guizao1712 <guilherme.leonardi.cruz@escola.pr.gov.br>
2026-08-03 11:54:58 -0300

    ...

---
+++ main.js
@@ -0,0 +1,9 @@
+document. querySelector('parametro-senha_botao');
+const numeroSenha = documentos.querySelector('.parametro-senha_texto');
+numeroSenha.textContent = 5;
+const botoes = document.querySelector('parametro.senha.botao')
+botoes{0}.onvlick = diminuirTamanho;
+console.console.log(botoes);
+function diminuirTamanho() {
+    
+}
